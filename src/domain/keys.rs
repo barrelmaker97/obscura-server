5dea2a1fbe8aa943c4cfbf960cb23605e55046c9
Nolan Cooper <nolancooper97@gmail.com>
2026-02-07 20:04:54 -0600

    Cleanup

--- src/domain/keys.rs
+++ src/domain/keys.rs
@@ -1,5 +1,4 @@
 use crate::domain::crypto::{PublicKey, Signature};
-use crate::error::{AppError, Result};
 
 #[derive(Debug, Clone)]
 pub struct SignedPreKey {
@@ -14,18 +13,10 @@ pub struct OneTimePreKey {
     pub public_key: PublicKey,
 }
 
-impl OneTimePreKey {
-}
-
 #[derive(Debug, Clone)]
 pub struct PreKeyBundle {
     pub registration_id: i32,
     pub identity_key: PublicKey,
     pub signed_pre_key: SignedPreKey,
     pub one_time_pre_key: Option<OneTimePreKey>,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
 }
