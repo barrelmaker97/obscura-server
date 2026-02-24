de3f9f838af4ce701d780a134f9c04231507b285
Nolan Cooper <nolancooper97@gmail.com>
2026-02-24 15:39:25 -0600

    Centralize

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,3 @@
-#![forbid(unsafe_code)]
-#![warn(clippy::all)]
-#![warn(clippy::pedantic)]
-#![warn(clippy::nursery)]
-#![warn(clippy::unwrap_used)]
-#![warn(clippy::todo)]
-#![warn(clippy::panic)]
-#![warn(clippy::dbg_macro)]
-#![warn(clippy::print_stdout)]
-#![warn(clippy::print_stderr)]
-#![warn(clippy::clone_on_ref_ptr)]
-#![warn(unreachable_pub)]
-#![warn(missing_debug_implementations)]
-#![warn(unused_qualifications)]
-#![deny(unused_must_use)]
-
 pub mod adapters;
 pub mod api;
 pub mod config;
